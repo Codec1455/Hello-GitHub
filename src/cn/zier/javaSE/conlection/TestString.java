7a1cfa0f4aefb2fefbd997cdfca0a302d5d8490c
Codec1455 <18739566372@163.com>
2020-07-30 13:58:12 +0800

    2020.7.30

--- src/cn/zier/javaSE/conlection/TestString.java
+++ src/cn/zier/javaSE/conlection/TestString.java
@@ -1,20 +1,40 @@
 package cn.zier.javaSE.conlection;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author mengshuaibin
  */
 public class TestString {
     public static void main(String[] args) {
-        System.out.println("MAX_MEMORY:"+byteToM(Runtime.getRuntime().maxMemory())+"M");
-        System.out.println("TOTAL_MEMORY:"+byteToM(Runtime.getRuntime().totalMemory())+"M");
-
+//        List list  = new ArrayList<>(5);
+//        list.add(11);
+//        list.add(11);
+//        list.add(11);
+//        list.add(11);
+//        Object o = list.get(1);
+//        int size = list.size();
+//        System.out.println(size);
+//        Set set = new HashSet();
+//        set.add(1);
+//        set.add(1);
+//        set.add(1);
+//        set.add(1);
+//        set.add(2);
+//        Iterator iterator = set.iterator();
+//        System.out.println(set.size());
+//        while (iterator.hasNext()){
+//            Object o = iterator.next();
+//            System.out.println(o);
+//        Map<Integer,Integer> map = new HashMap<>();
+//        map.put(1,1);
+//        map.put(2,2);
+//        map.put(3,3);
+//        map.put(4,4);
+//        map.put(5,5);
+//        }
+        Map<Object, Object> map = new ConcurrentHashMap<>();
     }
-    public static double round (long num ,int scale){
-        return Math.round(Math.pow(10,scale)*num)/Math.pow(10,scale);
-    }
-    static double byteToM(long num){
-        return round(num/1024/1024,2);
-    }
-
     }
 
