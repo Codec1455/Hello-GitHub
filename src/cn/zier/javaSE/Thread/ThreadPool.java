f58e65f13b37a5b52729e05afc1c90235340bcf9
Codec1455 <18739566372@163.com>
2020-07-30 13:58:34 +0800

    2020.7.30

---
+++ src/cn/zier/javaSE/Thread/ThreadPool.java
@@ -0,0 +1,33 @@
+package cn.zier.javaSE.Thread;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author mengshuaibin
+ */
+public class ThreadPool {
+    private static final int CORE_POOL_SIZE = 3;
+    private static final int MAX_POOL_SIZE = 5;
+    private static final int QUEUE_CAPACITY = 10;
+    private static final Long KEEP_ALIVE_TIME = 1L;
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor  = new ThreadPoolExecutor(
+                CORE_POOL_SIZE,
+                MAX_POOL_SIZE,
+                KEEP_ALIVE_TIME,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(QUEUE_CAPACITY),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+        for (int i = 0; i < 15; i++) {
+            Run r = new Run();
+            threadPoolExecutor.execute(r);
+        }
+        threadPoolExecutor.shutdown();
+        while (!threadPoolExecutor.isTerminated()) {
+        }
+        System.out.println("Finished all threads");
+    }
+    }
+
